e55d9a0bc25e9dd5046f6042b0e612d42d4e6e8d
RomanSibakovskij <j4023958@gmail.com>
2024-11-09 08:20:54 +0200

    Add empty SingleProductDashboardPage sheet

--- src/test/java/com/shinola/HomePageTest.java
+++ src/test/java/com/shinola/HomePageTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 public class HomePageTest extends TestMethods{
 
+    //Test 001 -> navigate to Circadian 36 Automatic' product dashboard page test method (as a guest)
     @Test
     @DisplayName("Navigate To 'Circadian 36 Automatic' Product Dashboard Page Test (as a guest)")
     void navigateToCircadian36ProductDashboardPageNoRegisterTest(){
